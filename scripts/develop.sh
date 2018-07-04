9d4ef6e48c265ccd5a86091525ab8d2775012e43
Zachary Dovel <zakkarido@gmail.com>
2018-07-04 08:36:10 -0700

    Make develop show debugger

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -9,7 +9,7 @@ STORYBOOK="$BIN_DIR/start-storybook"
 
 $SCRIPT_DIR/apply-test-results.js 2>&1 > /dev/null&
 
-$SCRIPT_DIR/test.sh -- --no-single-run 2>&1 > /dev/null&
+$SCRIPT_DIR/continuous.sh 2>&1 > /dev/null&
 PID=$?
 
 $STORYBOOK -p 6006 -c $PROJECT_DIR/.develop -s $PROJECT_DIR "$@"
