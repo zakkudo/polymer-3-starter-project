1e43b4335023856279bf1494c3c2c0a936436d92
Zachary Dovel <zakkarido@gmail.com>
2018-08-04 15:15:13 -0700

    Move native documentation into it's own package

--- src/documentation.js
+++ src/documentation.js
@@ -1,26 +1,5 @@
 
 /**
- * Native HTML/Javascript in the browser.
- * @typedef Native
- * @namespace Native
- */
-
-/**
- * The DocumentFragment interface represents a minimal document object that
- * has no parent.
- * @typedef Native.DocumentFragment
- * @memberof Native
- * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment}
- */
-
-/**
- * An event that is synthetically created and fired.
- * @memberof Native
- * @typedef Native.CustomEvent
- * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent}
- */
-
-/**
  * Generates a deferred promise that can be resolved or rejected externally.
  * @typedef {Object} DeferredPromise
  * @property {Promise} promise - The unresolved promise.
